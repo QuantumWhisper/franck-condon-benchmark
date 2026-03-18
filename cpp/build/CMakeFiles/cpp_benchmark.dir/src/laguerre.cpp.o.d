1f92ff8adef9091c78967359708b6992d8a853ab
Shanglong Ning <134532926+QuantumWhisper@users.noreply.github.com>
2026-03-18 10:32:50 +0000

    Remove C++ build artifacts from tracking, update .gitignore

--- cpp/build/CMakeFiles/cpp_benchmark.dir/src/laguerre.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cpp_benchmark.dir/src/laguerre.cpp.o: \
-  /Users/s/Documents/MATLAB/Franck-Condon\ simulation/cpp/src/laguerre.cpp \
-  /Users/s/Documents/MATLAB/Franck-Condon\ simulation/cpp/src/laguerre.hpp
